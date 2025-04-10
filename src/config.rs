4d452fdc2cf8df788ba3d459d426badc610ab1e4
maxmaxxiong <yeguixiong@live.com>
2025-04-10 10:02:02 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,7 +98,13 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+pub const PUBLIC_RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+
+pub const RENDEZVOUS_SERVERS: &str = match option_env!("RENDEZVOUS_SERVERS") {
+    Some(key) if !key.is_empty() => key,
+    _ => PUBLIC_RENDEZVOUS_SERVERS,
+};
+
 pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
